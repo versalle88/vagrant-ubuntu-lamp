d6314f64d36e2e7c7264a00ef4216979f65140a0
Andrew Versalle <andrew.versalle@gmail.com>
2020-03-22 17:55:38 -0400

    Installed Apache

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo -e "\n--------------------- Updating our local package index... ---------------------\n"
+apt-get update
+echo -e "\n--------------------- Finished updating our local package index. ---------------------\n"
+
+echo -e "\n--------------------- Installing Apache... ---------------------\n"
+apt-get install -y apache2
+echo -e "\n--------------------- Finished installing Apache. ---------------------\n"
